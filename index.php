46f8c7e5c95529dc64673668fd4d942bfc9d344b
joaotavaresmatos <117996562+joaotavaresmatos@users.noreply.github.com>
2023-12-05 23:37:41 -0300

    finalizado

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>cadastro joao</title>
     <link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/512/44/44973.png">
+    <link rel="shortcut icon" type="" href="/assets/img/TechLand (1).png ">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="login.css">
 </head>
